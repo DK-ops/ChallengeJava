b44380b8014379930e383878d9985c088af51de5
DK-ops <321andika@gmail.com>
2022-04-13 19:17:26 +0700

    final try catch

--- app/src/main/java/com/mobilepro/high_lower/MainActivity.java
+++ app/src/main/java/com/mobilepro/high_lower/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mobilepro.high_lower;
 
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -11,28 +14,45 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     int randomNumber;
-    public void randomNumberGenerator() //for repeating the game
+    public void randomNumberGenerator()
     {
         Random rand = new Random();
         randomNumber = rand.nextInt(20)+1;
     }
-    public void onGuess(View view) {
-        String message;
-        EditText editTextNumber = (EditText) findViewById(R.id.editTextNumber);
-        int guessedNumber = Integer.parseInt(editTextNumber.getText().toString());
-        if (guessedNumber < randomNumber)
-            message = "Higher !!";
-        else if (guessedNumber > randomNumber)
-            message = "Lower !!";
-        else {
-            message = "You got me !!";
-            randomNumberGenerator();    //kinda recurtion !
+    public void onNebak(View view)
+    {
+        String message = "";
+
+        try {
+
+            EditText editTextNumber = (EditText) findViewById(R.id.edtnumber);
+            int angkaDitebak = Integer.parseInt(editTextNumber.getText().toString());
+
+            if(angkaDitebak < randomNumber) {
+                message = "Angka Lebih Tinggi !!";
+            }
+            else if(angkaDitebak > randomNumber) {
+                message = "Angka Lebih Rendah !!";
+            }
+            else if (angkaDitebak == randomNumber)
+            {
+                message="You got me !!";
+                randomNumberGenerator();
+            }
+
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
         }
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        catch(Exception e) {
+            Toast.makeText(this, "Masukkan Harus Angka!!!", Toast.LENGTH_SHORT).show();
+        }
+
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Random rand = new Random();
+        randomNumber = rand.nextInt(20) + 1;    //limit upto 20 from 1
     }
 }
